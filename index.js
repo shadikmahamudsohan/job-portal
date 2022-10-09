b20e708f6e847607d8c4bd434a1a608ce225a5a2
shadik <shadikmahamudsohan@gmail.com>
2022-10-09 23:42:29 +0600

    port changed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.Db_PASS}@cl
 //     console.log(`Database connection is successful 🛢`.red.bold);
 // });
 // server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`.yellow.bold);
